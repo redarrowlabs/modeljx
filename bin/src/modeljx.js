9cedc519c34ae7a64cc590893372eddecd9a4efe
bovorasr <bovorasr@gmail.com>
2016-10-06 08:55:19 -0500

    Implements new projection registration

--- bin/src/modeljx.js
+++ bin/src/modeljx.js
@@ -1,17 +1,53 @@
+import * as Immutable from 'immutable';
 class ProjectionStage {
     constructor(from, to) {
+        this._projections = Immutable.Map();
         this.from = from;
         this.to = to;
     }
+    overrideProperty(fromProperty, toProperty, use) {
+        this._projections = this._projections.set(`${fromProperty}|${toProperty}`, use);
+    }
     override(props) {
         props;
+        let { fromProperty, toProperty, forProperty, use } = props;
+        let from = '';
+        let to = '';
+        if (fromProperty) {
+            from = ProjectionStage.extractPropertyName(fromProperty);
+            to = ProjectionStage.extractPropertyName(toProperty);
+            this.overrideProperty(from, to, use);
+        }
+        else if (forProperty) {
+            from = to = ProjectionStage.extractPropertyName(forProperty);
+        }
+        else {
+            throw "unsupported projection type";
+        }
+        this.overrideProperty(from, to, use);
         return this;
+    }
+    // ~todo~ Add error checking and make this more safe.
+    static extractPropertyName(fromProperty) {
+        let serialized = fromProperty.toString();
+        let isLambda = serialized.indexOf('=>') > -1;
+        return isLambda
+            ? serialized.split('=>')[1].trim().split('.').splice(-1, 1)[0]
+            : serialized.split('return')[1].split(';')[0].split('.')[1].trim();
     }
     build() {
         return (from) => {
             if (from) {
             }
-            return this.to({});
+            var result = this.to({});
+            // 1. loop through every property on the 'from' object.
+            for (let propertyName in from) {
+                // 2. attempt to lookup a specific projection for that property and execute it.
+                let hasProjection = false;
+                if (hasProjection) {
+                }
+            }
+            return result;
         };
     }
 }
--- src/modeljx.ts
+++ src/modeljx.ts
@@ -19,12 +19,21 @@ class ProjectionStage<TFromType, TResultType> implements IProjectionStage<TFromT
     private from: (o: any) => TFromType;
     private to: (o: any) => TResultType;
 
+    private _projections: Immutable.Map<string, Function> = Immutable.Map<string, Function>();
+
     constructor(
         from: (o: any) => TFromType,
         to: (o: any) => TResultType
     ) {
         this.from = from;
         this.to = to;
+    }
+
+    private overrideProperty(
+        fromProperty: string,
+        toProperty: string,
+        use: (from: any) => any) {
+        this._projections = this._projections.set(`${fromProperty}|${toProperty}`, use);
     }
 
     override(props: {
@@ -38,15 +47,55 @@ class ProjectionStage<TFromType, TResultType> implements IProjectionStage<TFromT
            [index : string] : any;
         }): IProjectionStage<TFromType, TResultType> {
             props;
+            let {fromProperty, toProperty, forProperty, use} = props;
+            let from = '';
+            let to = '';
+            if (fromProperty) {
+                from = ProjectionStage.extractPropertyName(fromProperty);
+                to = ProjectionStage.extractPropertyName(toProperty);
+                this.overrideProperty(from, to, use);
+            } else if (forProperty) {
+                from = to = ProjectionStage.extractPropertyName(forProperty);
+            } else {
+                throw "unsupported projection type";
+            }
+            this.overrideProperty(from, to, use);
             return this;
+    }
+
+    // ~todo~ Add error checking and make this more safe.
+    private static extractPropertyName(fromProperty: Function) : string {
+        let serialized: string = fromProperty.toString();
+        let isLambda = serialized.indexOf('=>') > -1;
+        return isLambda
+            ? serialized.split('=>')[1].trim().split('.').splice(-1, 1)[0]
+            : serialized.split('return')[1].split(';')[0].split('.')[1].trim();
     }
 
     build(): (from: TFromType) => TResultType {
         return (from: TFromType) => {
-            if (from) {
 
+
+            if (from) {
+                // placeholder.
             }
-            return this.to({});
+
+            var result = this.to({});
+            // 1. loop through every property on the 'from' object.
+            for (let propertyName in from) {
+                // 2. attempt to lookup a specific projection for that property and execute it.
+                let hasProjection: boolean = false;
+                if (hasProjection) {
+                    // execute it.
+                }
+                // 3. if there aren't any, attempt to find a matching property on the 'to' object.
+                // if (result[propertyName]) {
+                //
+                // }
+
+                // 4. If nothing's found, skip it.
+            }
+            return result;
         };
     }
 }
